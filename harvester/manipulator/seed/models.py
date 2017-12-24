f1d41be5f2587280770f179458dd7f78be5534c0
Aforwardz <aforwardzyu@gmail.com>
2017-12-24 16:07:55 +0800

    seed model

--- harvester/manipulator/seed/models.py
+++ harvester/manipulator/seed/models.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 from django.db import models
 
 # Create your models here.
@@ -9,19 +10,21 @@ class SeedManager(models.Manager):
 
 class Seed(models.Model):
     create_time = models.DateTimeField(
-        verbose_name='',
+        verbose_name='创建时间',
         auto_now_add=True
     )
     update_time = models.DateTimeField(
-        verbose_name='',
+        verbose_name='更新时间',
         auto_now=True
     )
 
     content = models.TextField(
-        verbose_name='',
+        verbose_name='文本',
         blank=True
     )
     clean_content = models.TextField(
-        verbose_name='',
+        verbose_name='清洗文本',
         blank=True
     )
+
+    # word
