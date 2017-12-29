a36a01ff5cc3d181f6abd2e7a8701c14b7b3a508
yucongwei <aforwardzyu@gmail.com>
2017-12-29 18:12:20 +0800

    add peeler--parse the text

--- harvester/manipulator/manipulator/urls.py
+++ harvester/manipulator/manipulator/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+admin.site.site_header = '「Harvester」'
 
 urlpatterns = [
     path(r'jet/', include(('jet.urls', 'jet'), 'jet')),
