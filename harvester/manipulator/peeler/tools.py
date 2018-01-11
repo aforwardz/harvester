8c09285d337ee8476a6cecf8d817f85c796b6822
yucongwei <aforwardzyu@gmail.com>
2018-01-11 18:42:36 +0800

    add dictionary builder

--- harvester/manipulator/peeler/tools.py
+++ harvester/manipulator/peeler/tools.py
@@ -3,26 +3,21 @@ import jieba
 import thulac
 from string import punctuation
 all_punctuation = punctuation + """～！￥…×（）【】、—《》？“”："""
+end_punc = '.?!。！？'
+thu = thulac.thulac(seg_only=True)
 
 
 def cut_to_sentences_and_tokenize(paragraph):
-    thu = thulac.thulac(seg_only=True)
-    end_punc = '.?!。！？'
     ret = []
     sentence = ''
-    max_length = 0
     for item in paragraph:
         sentence += item
         if item in end_punc:
-            tokenize = thu.cut(sentence, text=True).split(' ')
-            max_length = max((max_length, len(tokenize)))
-            ret.append(tokenize)
+            ret.append(thu.cut(sentence, text=True).strip().split(' '))
             sentence = ''
     # 还需考虑句子中没有这些标点符号的case
     if sentence:
-        tokenize = thu.cut(sentence, text=True).split(' ')
-        max_length = max((max_length, len(tokenize)))
-        ret.append(tokenize)
+        ret.append(thu.cut(sentence, text=True).strip().split(' '))
     index = 0
     # 去除只包含标点符号的行
     while index < len(ret):
@@ -32,4 +27,4 @@ def cut_to_sentences_and_tokenize(paragraph):
         else:
             index += 1
 
-    return max_length, ret
+    return ret
