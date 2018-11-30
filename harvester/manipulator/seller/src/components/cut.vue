039c408038a15b848cb175b645f54cb7c77ec4ed
Aforwardz <aforwardzyu@gmail.com>
2018-11-30 14:25:46 +0800

    update seller style

--- harvester/manipulator/seller/src/components/cut.vue
+++ harvester/manipulator/seller/src/components/cut.vue
@@ -72,15 +72,16 @@ export default {
     height: auto;
     border-radius: 10px;
   }
-  .el-textarea {
+  .content-input-container .el-textarea {
     width: 60%;
+    height: auto;
   }
-  .cut-button-container {
+  .content-input-container .cut-button-container {
     width: 10%;
     height: auto;
     margin: auto;
   }
-  .el-button--primary {
+  .cut-button-container .el-button--primary {
     color: hotpink;
     background-color: lightblue;
     width: 100%;
