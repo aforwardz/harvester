0b4c29af4be2c0443a26c1eebf36ec68181770d3
Aforwardz <aforwardzyu@gmail.com>
2018-12-06 21:02:36 +0800

    add extractor in processor

---
+++ harvester/manipulator/processor/choices.py
@@ -0,0 +1,25 @@
+LABEL_PAIRS = (
+    (('球队', '队伍', '队'), 'Team'),
+    (('俱乐部',), 'Club'),
+    (('国家队',), 'NationTeam'),
+    (('队友', '球员'), 'Player'),
+    (('教练',), 'Coach'),
+)
+
+RELATION_PAIRS = (
+    (('球队', '队伍', '队'), 'Team'),
+    (('俱乐部',), 'Club'),
+    (('国家队',), 'NationTeam'),
+    (('队友', '球员'), 'Player'),
+    (('教练',), 'Coach'),
+)
+
+INTENT_PAIRS = (
+    (('球队', '队伍', '队'), 'Team'),
+    (('俱乐部',), 'Club'),
+    (('国家队',), 'NationTeam'),
+    (('队友', '球员'), 'Player'),
+    (('教练',), 'Coach'),
+)
+
+MULTIPLE_WORDS = ('哪些', '')
